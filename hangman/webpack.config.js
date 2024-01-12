515f27b68c1fadfb74ca0b028df740cf57e67d87
Alexandra <iammeduza@gmail.com>
2024-01-12 13:52:20 +0300

    fix: edit config files for propper linting and bundling

--- hangman/webpack.config.js
+++ hangman/webpack.config.js
@@ -3,7 +3,22 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'src'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+        '.js'
+    ]
+  },
+  mode: 'development'
 };
