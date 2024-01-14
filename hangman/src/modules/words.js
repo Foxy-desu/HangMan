c56e13e898a4fb2c03b0a27a5ad197c388c48325
Alexandra <iammeduza@gmail.com>
2024-01-14 15:15:17 +0300

    feat: create words object

---
+++ hangman/src/modules/words.js
@@ -0,0 +1,52 @@
+export const words = [
+    {
+        wordId: 0,
+        wordHint: 'a tool to put all your js modules together',
+        word: 'bundler',
+    },
+    {
+        wordId: 1,
+        wordHint: 'a reusable piece of code which exposes public API and keeps implementation details incapsulated',
+        word: 'module',
+    },
+    {
+        wordId: 2,
+        wordHint: 'a tool that analyzes source code for errors, vulnerabilities, and stylistic issues to improve code quality',
+        word: 'linter',
+    },
+    {
+        wordId: 3,
+        wordHint: 'structure that provides a base for the application development process',
+        word: 'framework',
+    },
+    {
+        wordId: 4,
+        wordHint: 'sequence of instructions that is interpreted or carried out by another program',
+        word: 'script',
+    },
+    {
+        wordId: 5,
+        wordHint: 'moving of function declarations to the top of their scope',
+        word: 'hoisting',
+    },
+    {
+        wordId: 6,
+        wordHint: 'propagation of an event up to ancestor elements',
+        word: 'bubbling',
+    },
+    {
+        wordId: 7,
+        wordHint: 'combination of a function enclosed with references to its surrounding state',
+        word: 'closure',
+    },
+    {
+        wordId: 8,
+        wordHint: 'current context of code, which determines the accessibility of variables',
+        word: 'scope',
+    },
+    {
+        wordId: 9,
+        wordHint: 'named abstract storage location for a value that can change',
+        word: 'variable',
+    }
+];
