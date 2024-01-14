86b76e51428a3e6932d14ac3d8110bbced00c2be
Alexandra <iammeduza@gmail.com>
2024-01-14 15:19:47 +0300

    feat: implement a moule with dom elements

---
+++ hangman/src/modules/dom.js
@@ -0,0 +1,42 @@
+import {createLetterBlocks, createButtons} from './functions';
+
+export const elems = () => {
+  const body = document.body;
+  const wrap = document.createElement('div');
+  const header = document.createElement('header');
+  const main = document.createElement('main');
+  const imageWrap = document.createElement('div');
+  const h1 = document.createElement('h1');
+  const image = document.createElement('img');
+  const wordWrap = document.createElement('div');
+  const letterBlocks = createLetterBlocks(7);
+  const hintWrap = document.createElement('div');
+  const wordHint = document.createElement('span');
+  const guessHint = document.createElement('p');
+  const guessHintSpan = document.createElement('span');
+  const keyboard = document.createElement('div');
+  const buttons = createButtons();
+
+  return (
+    {
+      body, 
+      wrap, 
+      header, 
+      main, 
+      imageWrap,
+      h1, 
+      image,
+      wordWrap,
+      letterBlocks,
+      hintWrap,
+      wordHint,
+      guessHint,
+      guessHintSpan,
+      keyboard,
+      buttons,
+    }
+  )
+};
+
+
+
