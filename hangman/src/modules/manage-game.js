82320636c3da0cb348b8cb8e13dca85dd1351589
Alexandra <iammeduza@gmail.com>
2024-01-14 15:26:52 +0300

    feat: implement a manageGame function as a module

---
+++ hangman/src/modules/manage-game.js
@@ -0,0 +1,72 @@
+import { chooseWord, createLetterBlocks } from "./functions";
+
+export function manageGame(wordObj, elements) {
+    let currentWord = chooseWord(wordObj);
+    let wordLen = currentWord.word.length;
+  
+    let mistakes = 0;
+    let totalPossibleMistakes = 6; //equals the number of bodyparts of the hangman
+    let rightAnswers = 0;
+  
+    let currentLetter;
+
+    function insertWordBlocks() {
+        const letterBlocks = createLetterBlocks(wordLen);
+        elements.wordWrap.innerHTML = letterBlocks;
+    };
+    function insertWordHint() {
+    const hint = currentWord.wordHint;
+    elements.wordHint.innerText = hint;
+    };
+    function insertGuessHint() {
+    const wrong = mistakes;
+    const total = totalPossibleMistakes;
+    elements.guessHintSpan.innerText = `${wrong} / ${total}`;
+    };
+    function countChecker() {
+        elements.guessHintSpan.innerText = `${mistakes} / ${totalPossibleMistakes}`;
+        function blockKeys() {
+            Array.from(elements.keyboard.children).forEach((key) => {
+                key.classList.add('keyboard__button_active');
+                key.setAttribute('disabled','');
+            })
+        }
+    //runs on btn click
+    if(mistakes === totalPossibleMistakes) {
+        blockKeys();
+        setTimeout(resetGame, 5000);
+        //run function to create a modal window for loosing the game
+        //with a button to start a new game;
+    }
+    if(rightAnswers === wordLen) {
+        blockKeys();
+        setTimeout(resetGame, 5000);
+        //run a function to create a modal for winning the game
+        //with a button to start a new one;
+    }
+    else return;
+    };
+    function letterChecker() {
+    const letterBlocks = elements.wordWrap.children;
+    const wordArr = currentWord.word.toUpperCase().split('');
+        
+    if (wordArr.includes(currentLetter)) {
+        wordArr.forEach((letter, index) => {
+        if (currentLetter === letter) {
+            letterBlocks[index].innerText = currentLetter.toLowerCase();
+                rightAnswers ++;
+                console.log(rightAnswers);
+            }
+        }
+        );
+    } else {
+        mistakes ++;
+        console.log(mistakes);
+    }
+    };
+     
+    insertWordBlocks();
+    insertWordHint();
+    insertGuessHint(); 
+  
+}
