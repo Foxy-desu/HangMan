bece5ade4217163e2a79acd2001b7cef54619076
Alexandra <iammeduza@gmail.com>
2024-01-15 15:32:07 +0300

    fix: replace image with canvas; feat: add canvas initializing

--- hangman/src/modules/initialize.js
+++ hangman/src/modules/initialize.js
@@ -1,17 +1,22 @@
+import { canvasCreator } from "./canvas";
+
 export function initialize(props) {
+  const {initialDrawing} = canvasCreator(props.canvas);
+
   function applyClass() {
     props.wrap.className = "wrap";
     props.header.className = "hero";
     props.main.className = "main";
     props.imageWrap.className = "hero__image-wrap";
     props.h1.className = "hero__header";
-    props.image.className = "hero__image";
+    // props.image.className = "hero__image";// replace this element with canvas
     props.wordWrap.className = "word-wrap";
     props.hintWrap.className = "hint-wrap";
     props.wordHint.className = "hint-wrap__word-hint";
     props.guessHint.className = "hint-wrap__guess-hint";
     props.guessHintSpan.className = "guess-hint__mistakes-count";
     props.keyboard.className = "keyboard";
+    props.canvas.className = "hero__canvas";
   }
   function applyText() {
     props.h1.innerText = "Hangman Game";
@@ -20,9 +25,9 @@ export function initialize(props) {
     props.guessHint.innerText = "Incorrect guesses: ";
     props.guessHintSpan.innerText = " / ";
   }
-  function applySrc() {
-    props.image.src = "./assets/images/gallows.svg";
-  }
+  // function applySrc() {
+  //   props.image.src = "./assets/images/gallows.svg";
+  // }
   function adjustHtml() {
     props.wordWrap.insertAdjacentHTML("afterbegin", props.letterBlocks);
     props.keyboard.insertAdjacentHTML("afterbegin", props.buttons);
@@ -31,7 +36,7 @@ export function initialize(props) {
     props.body.append(props.wrap);
     props.wrap.append(props.header, props.main);
     props.header.append(props.imageWrap, props.h1);
-    props.imageWrap.append(props.image);
+    props.imageWrap.append(props.canvas); //replaced image with canvas
     props.main.append(props.wordWrap, props.hintWrap, props.keyboard);
     props.hintWrap.append(props.wordHint, props.guessHint);
     props.guessHint.append(props.guessHintSpan);
@@ -39,7 +44,8 @@ export function initialize(props) {
 
   applyClass();
   applyText();
-  applySrc();
+  // applySrc();
   adjustHtml();
   insertElems();
+  initialDrawing();
 }
